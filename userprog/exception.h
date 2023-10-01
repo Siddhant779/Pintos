e19d71f889e4adf21ac50899b0e7d747d9357f99
Siddhant779 <panditsiddhant.sp@utexas.edu>
2023-10-01 15:03:22 -0500

    added page_fault handler
    Signed-off-by: Siddhant779 <panditsiddhant.sp@utexas.edu>

--- userprog/exception.h
+++ userprog/exception.h
@@ -5,8 +5,11 @@
 #define PF_P 0x1 /* 0: not-present page. 1: access rights violation. */
 #define PF_W 0x2 /* 0: read, 1: write. */
 #define PF_U 0x4 /* 0: kernel, 1: user process. */
+#include "threads/interrupt.h"
 
 void exception_init(void);
 void exception_print_stats(void);
+
+void page_fault_handler(struct intr_frame *f);
 
 #endif /* userprog/exception.h */
