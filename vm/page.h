ccb5b28a7dc437b99ef5b2a2779237085efced1f
David Li <lidavid216@gmail.com>
2023-10-13 17:09:14 -0500

    updated SPTE struct

--- vm/page.h
+++ vm/page.h
@@ -7,11 +7,13 @@
 // Store a list of pages for each file (each file is divided into various pages spread around memory)
 // Each thread should have an SPT (Hash table, which stores SPTE data structures)
 struct SPTE {
-    struct hash_elem hash_elem; // Hash value for each page, lets it be part of the hash table
+    struct hash_elem SPTE_hash_elem; // Hash value for each page, lets it be part of the hash table
     // the hash is within the thread.h and each SPT is going to have a SPTE
     //the key for the hash could be the address of the page, fault address, or the page number 
     struct file *file; 
     uint8_t *kpage;
+    size_t page_read_bytes;
+    size_t page_zero_bytes;
     uint8_t isDirty; //Indicates of page has been modified or not
 }
 
