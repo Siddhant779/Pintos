d5ae60e049faa66f83794afb2c1f4a6837db2296
Siddhant779 <panditsiddhant.sp@utexas.edu>
2023-10-20 02:18:50 -0500

    just added SPT and made a few changes
    Signed-off-by: Siddhant779 <panditsiddhant.sp@utexas.edu>

--- vm/page.c
+++ vm/page.c
@@ -1,9 +1,18 @@
-
+#include <hash.h>
+#include <string.h>
+#include "page.h"
 //Each thread should init this, stores SPT (Supplemental Page Table)
 void SPT_init(struct hash *table){
     hash_init(); //should have 4 parameters - table, page hash, precede page 
 }
 
+struct SPT* SPT_init(void) {
+struct SPT *SPT =
+    (struct SPT*) malloc(sizeof(struct SPT));
+
+  hash_init (&SPT->page_entries, spte_hash_func, spte_less_func, NULL);
+  return SPT;
+}
 //need hash value for page 
 
 //need something to check if a page precedes 
--- vm/page.h
+++ vm/page.h
@@ -3,18 +3,36 @@
 
 #include "hash.h"
 
+enum page_status {
+    FRAME, // in the memory
+    SWAP, //on the swap table 
+    ALL_ZERO // all zeros 
+    //am i missing something else ?
+}; 
 // Supplemental Page Table Entry
 // Store a list of pages for each file (each file is divided into various pages spread around memory)
 // Each thread should have an SPT (Hash table, which stores SPTE data structures)
+struct SPT {
+    struct hash page_entries;
+};
+
 struct SPTE {
     struct hash_elem SPTE_hash_elem; // Hash value for each page, lets it be part of the hash table
     // the hash is within the thread.h and each SPT is going to have a SPTE
     //the key for the hash could be the address of the page, fault address, or the page number 
     struct file *file; 
-    uint8_t *kpage;
+
+    void *kpage; // frame associated with this page  - could be null if there is no frame 
+    void *upage; // for hte virutal address of hte page this exists as the key 
+
     size_t page_read_bytes;
     size_t page_zero_bytes;
-    uint8_t isDirty; //Indicates of page has been modified or not
-}
+
+    bool isDirty; //Indicates of page has been modified or not
+    bool writeable;
+
+    enum page_status page_stat;
+    // we need something for the swap here 
+};
 
 #endif
