3e99533470ecb417f4562cdae192eac081de20a3
David Li <lidavid216@gmail.com>
2023-10-21 23:10:54 -0500

    updated eviction and started get_page

--- vm/frame.h
+++ vm/frame.h
@@ -7,15 +7,15 @@
 #include "palloc.h"
 
 const int FRAME_NUM = 367;
-struct frame {
-    void *frame_page; //pointer to the place in memory
+struct FTE {
+    void *frame; //pointer to the actual "frame" of data itself
     struct list processes; //list of processes associated with the frame
     struct SPTE *page_entry; //pointer to page table entry associated to the frame
 };
 
 struct bitmap *frame_map; //to be initialized in init
 
-struct frame frame_table[FRAME_NUM];  //array of all frames
+struct FTE frame_table[FRAME_NUM];  //array of all frames
 
 int evict_start; //points to the index of the last evicted frame + 1, or 0 by default
 
@@ -28,7 +28,7 @@ void frame_init(); //initialize frame table and frame bitmap
 - want SPTE of the page associated in the frame you are loading in
 - pass no-grow pt cases when you implement frame allocation. The grow cases are passed with stack growth
 */
-struct frame *get_frame();
+void *get_frame();
 
 int evict_frame(); //eviction algorithm, should return the index of the frame corresponsing to the page that was evicted
 
