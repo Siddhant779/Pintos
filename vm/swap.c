1f05ba739041dd3e81bd447d12eb23f6392c18b2
Siddhant779 <panditsiddhant.sp@utexas.edu>
2023-11-10 16:06:00 -0600

    cleaned up code and added comments
    Signed-off-by: Siddhant779 <panditsiddhant.sp@utexas.edu>

--- vm/swap.c
+++ vm/swap.c
@@ -2,7 +2,7 @@
 
 
 static struct bitmap *block_tracker;
-static struct block *swap_slot;
+static struct block *swap_block;
 
 static const size_t SWAP_SLOT_SIZE = PGSIZE / BLOCK_SECTOR_SIZE;
 
@@ -11,10 +11,13 @@ static size_t swap_size;
 
 
 void swap_init(void) {
-    swap_slot = block_get_role(BLOCK_SWAP);
-    swap_size = block_size(swap_slot) / SWAP_SLOT_SIZE;
+    //Get swap block (entire swap area 4MB)
+    swap_block = block_get_role(BLOCK_SWAP);
+    //Get size of the whole swap area in units of sectors (Probably a very large number since the entire block device can store many sectors)
+    swap_size = block_size(swap_block) / SWAP_SLOT_SIZE;
+     //Create bitmap of to track of all the swap slots in the swap area (each slot has 8 sectors)
     block_tracker = bitmap_create(swap_size);
-
+    // if its true that means its free; false means its occupied 
     bitmap_set_all(block_tracker, true);
 }
 
@@ -24,10 +27,12 @@ int putInSwapArea(void *k_page) {
         lock_release(&sys_lock);
     }
     lock_acquire(&sys_lock);
+     //Read all slots in swap area, find a slot in swap area to put the frae
     size_t swap_avail = bitmap_scan_and_flip(block_tracker, 0, 1, true);
-
+     //Write to 8 sectors. This takes whatever data from frame and put it on the swap area
     for(int i = 0; i < SWAP_SLOT_SIZE; i++) {
-        block_write(swap_slot, swap_avail * SWAP_SLOT_SIZE + i, k_page + (BLOCK_SECTOR_SIZE * i));
+        //Write data 
+        block_write(swap_block, swap_avail * SWAP_SLOT_SIZE + i, k_page + (BLOCK_SECTOR_SIZE * i));
 
     }
     lock_release(&sys_lock);
@@ -38,6 +43,7 @@ int putInSwapArea(void *k_page) {
     return swap_avail;
 }
 
+//Function for swapping a frame with something on swap table. When you put a page into swap area, SPTE should store the index of it so you can get it back later
 void getFromSwapArea(int swap_idx, void *k_page) {
     bool has_lock = lock_held_by_current_thread(&sys_lock);
     if(has_lock) {
@@ -47,8 +53,10 @@ void getFromSwapArea(int swap_idx, void *k_page) {
     ASSERT(swap_idx < swap_size);
 
     if(bitmap_test(block_tracker, swap_idx) == false) {
+        //Read from 8 sectors so the frame can be obtained from swap area
        for(int i = 0; i < SWAP_SLOT_SIZE; i++) {
-            block_read(swap_slot, swap_idx * SWAP_SLOT_SIZE + i, k_page + (BLOCK_SECTOR_SIZE * i));
+            //Read data (not sure what the purpose of each parameter here is)
+            block_read(swap_block, swap_idx * SWAP_SLOT_SIZE + i, k_page + (BLOCK_SECTOR_SIZE * i));
         }
     }
     bitmap_mark(block_tracker,swap_idx);
